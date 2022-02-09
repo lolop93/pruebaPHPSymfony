5b6bfc5848964f09554f34f157bbd5e697387743
Manuel Prieto Macias <lolop_1993@hotmail.com>
2022-02-09 23:50:17 +0100

    installation of symfony 6

--- Services/NotificationService.php
+++ Services/NotificationService.php
@@ -7,9 +7,7 @@ class NotificationService
     public function notify (): string
     {
         $messages = [
-            'You did it! You updated the system! Amazing!',
-            'That was one of the coolest updates I\'ve seen all day!',
-            'Great work! Keep going!',
+            
         ];
 
         $index = array_rand($messages);
